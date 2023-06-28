61d61257b1d83a7ef8deaeab8c747a8c03cde6b4
elrilacho <camilobr17@gmail.com>
2023-06-28 12:10:52 -0500

    tema8_listo

--- src/views/Tema7.vue
+++ src/views/Tema7.vue
@@ -5,8 +5,56 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 7
-      h1 
-
+      h1 Áreas funcionales
+    .row.justify-content-center.mb-5
+      .col-lg-7.mb-lg-0.mb-3
+        p Una empresa es una organización socioeconómica, lo cual significa relacionar personas, actividades y dinero. En medio de esa relación se realizan una serie de actividades, necesarias para cumplir con unos objetivos y propósitos previamente establecidos.
+        .p-5(style="background-color: #FEE7DD ")
+          p.mb-0 Para lograr caracterizar y clasificar esas actividades, se identifican las áreas y departamentos donde se realizará cada una, esto es importante para poder intervenirlas de manera individual (cuando así se requiera) y se gana en efectividad.
+      .col-lg-5.col-8: img(src='@/assets/curso/temas/tema7/1.png', alt='')
+    p.mb-5(data-aos='fade-right') De acuerdo con la naturaleza de las empresas y con su actividad económica, se organizan y/o discriminan estas actividades, departamentos, áreas o funciones:
+    .row.justify-content-center.mb-5
+      .col-lg-3.col-8: img(src='@/assets/curso/temas/tema7/2.png', alt='')
+      .col-lg-9
+        .acordion-2
+          AcordionA.mb-5(tipo="a" clase-tarjeta="tarjeta tarjeta--azul")
+            div(titulo="Área de Gestión Financiera")
+              p.mb-0
+            div(titulo="Área de Recursos Humanos")
+              p.mb-0
+            div(titulo="Mercadeo y Ventas")
+              p.mb-0 Estos están estrechamente vinculados a los procesos y la operación de la empresa en su conjunto. Los indicadores operativos se asignan a los equipos de trabajo o a las personas directamente, involucrando a cada empleado de la empresa de tal manera que contribuyan activamente a los objetivos estratégicos de la organización, para seguir con nuestro ejemplo en este caso propondremos un indicador operativo como el número de productos fabricados por módulo o por persona, en donde también tendría como objetivo aumentar, para asegurar el cumplimiento de los indicadores tácticos y estratégicos.
+            div(titulo="Área de Producción")
+              p.mb-0
+    .row.justify-content-center.mb-5
+      .col-lg-6.mb-lg-0.mb-3
+        .p-5(style="background-color: #FEE7DD").h-100
+          img(src='@/assets/curso/temas/tema7/3.svg', alt='' style="width: 85px").mb-3
+          h5 ¡Funciones de las áreas!
+          p Cada una de estas áreas tiene unas funciones y responsabilidades definidas y, entendiéndolas como un sistema, deben funcionar. El cumplimiento de los objetivos depende del desempeño de cada una de las áreas, del grado de cumplimiento, del mejor uso de los recursos con que disponen, y de lograr mejor satisfacción de los clientes.
+      .col-lg-6.mb-lg-0.mb-3
+        .p-5(style="background-color: #F5E6E9").h-100
+          img(src='@/assets/curso/temas/tema7/4.svg', alt='' style="width: 85px").mb-3
+          h5 ¡Cuadros comparativos!
+          p De acuerdo con las funciones que tiene cada área se pueden hacer cuadros comparativos entre lo que se espera y lo que realmente se logra, teniendo en cuenta cuál es la actividad más representativa de esa área. De esta manera, se establece cuál va a ser el indicador más representativo, y con ese criterio, pasar a calificar.
+    .row.justify-content-center.align-items-center.mb-5
+      .col-lg-8.mb-lg-0.mb-3
+        //todo! Añadir documento 
+        .tarjeta.color-secundario.p-3.mb-4
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/temas/tema3/31.svg").img65
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  h4.mb-1 Desempeño de áreas funcionales
+                  p.mb-0.text-small Le invitamos a consultar el siguiente recurso donde se presentan algunos ejemplo de desempeño de las diferente áreas.		
+                .col-sm-auto
+                  a.boton.color-acento-botones(href="https://docs.google.com/document/d/1wD3oByzOhsH4c0oWnUb4DWNBc0hmCOhrMDWG0-nCtyo/edit?usp=sharing" target="_blank")
+                    span Descargar
+                    i.fas.fa-file-download
+      .col-lg-4: img(src='@/assets/curso/temas/tema7/5.png', alt='')
+            
 </template>
 
 <script>
@@ -26,4 +74,10 @@ export default {
 }
 </script>
 
-<style lang="sass"></style>
+<style lang="sass">
+.acordion-2
+  .acordion__activo
+    background-color: #ABF6F8 !important
+.tarjeta--azul
+  background-color: #D9FBFC
+</style>
--- src/views/Tema8.vue
+++ src/views/Tema8.vue
@@ -5,7 +5,60 @@
     .titulo-principal.color-acento-contenido
       .titulo-principal__numero
         span 8
-      h1 
+      h1 Canales de comunicación
+    .row.justify-content-center.mb-5
+      .col-lg-4.mb-lg-0.mb-3
+        p Los procesos de comunicación a nivel empresarial se han transformado gracias al mundo digital, es una realidad que la gestión de la comunicación es inseparable de la gestión organizacional, por eso implica la planificación, administración y evaluación de los flujos comunicativos, es decir, saber comunicarse a nivel organizacional representa un valor competitivo, ya que genera un impacto positivo en el desarrollo empresarial.
+      .col-lg-8
+        .row.justify-content-center.mb-5.g-0
+          .col-lg-4
+            img(src='@/assets/curso/temas/tema8/1.png', alt='')
+          .col-lg-8
+            .p-4(style="background-color: #EFFEFE").h-100
+              p.mb-0 De manera elemental, se podría decir que los canales de comunicación son las vías, soportes, caminos, herramientas y demás elementos necesarios para compartir mensajes e información. En las organizaciones se asumen responsabilidades a través de la socialización de la información pertinente a cada función, cargo y perfil.
+                br
+                br
+                | #[b Para esto se requiere de una plataforma que asegure total conectividad, integración, información, participación y solidaridad.]
+    .row.justify-content-center.mb-5
+      .col-lg-4.mb-lg-0.mb-3: img(src='@/assets/curso/temas/tema8/2.png', alt='')
+      .col-lg-8
+        p Sobre los procesos de comunicación en las empresas u organizaciones, tenga en cuenta aspectos como:
+        AcordionA.mb-5(tipo="b" clase-tarjeta="tarjeta tarjeta--red")
+          div(titulo="Cantidad y calidad de la información")
+            p.mb-0 La cantidad y la calidad de la información que se comunica entre las áreas funcionales de las organizaciones es importante en la medida que las reciban las personas que son, en el momento oportuno, que no estén incompletas, que sean veraces, y lo más valioso, que sea información pertinente y funcional.
+          div(titulo="Aprovechamiento y uso de los datos")
+            p.mb-0 Para mostrar un buen desempeño en las áreas funcionales es fundamental aprovechar de manera idónea los datos que contengan información importante, y que se compartan entre sí. Para esto, los canales utilizados para que esa información se desplace, y sea bien utilizada, son cruciales.
+          div(titulo="Comunicación e indicadores")
+            p.mb-0 Para evaluar el desempeño es indispensable conocer cifras de entradas, de procesos, y de resultados, todos estos indicadores ayudan a tomar decisiones acertadas, y se está cumpliendo con uno de los objetivos: realizar análisis para tomar las mejores acciones de mejora.
+          div(titulo="Estructuras de red e intercomunicación")
+            p.mb-0 Los datos solos o aislados no constituyen información, deben estar estructurados en una red o un sistema que les permita interactuar y convertirse en informes útiles.
+    .row.justify-content-center.mb-5
+      .col-lg-8.mb-lg-0.mb-3
+        p Sobre los procesos de comunicación en las empresas u organizaciones, tenga en cuenta aspectos como:
+        AcordionA.mb-5(tipo="b" clase-tarjeta="tarjeta tarjeta--red")
+          div(titulo="Frecuencia de uso")
+            p.mb-0 Un sistema de comunicaciones o de información se ve justificado cuando la frecuencia con que se utiliza es alta y cuando los datos que transitan por allí son realmente utilizados, e intervienen el mejoramiento en la productividad y el cumplimiento de metas.
+          div(titulo="Técnicas de comunicación")
+            p.mb-0 Al evaluar el desempeño laboral se hace necesario emitir y dirigir un mensaje a los participantes, dando lineamientos claros sobre qué se espera de ellos, de su desempeño y de sus obligaciones. En la comunicación está la clave del éxito o el fracaso de un proceso de evaluación de desempeño.
+          div(titulo="Formas comunicativas")
+            p.mb-0 La comunicación, tanto escrita como verbal, es el elemento diferenciador de una gestión del rendimiento eficaz. Por eso se plantea que las principales funciones de la comunicación a nivel empresarial están relacionadas con manejo de información, motivación y control.
+          div(titulo="Comunicación para la satisfacción")
+            p.mb-0 Concebir un escenario de satisfacción, control, seguridad y cooperación a través de procesos eficaces de comunicación será una estrategia inestimable de mejoramiento y desarrollo del trabajador y por lo tanto de eficacia en la productividad.
+        //todo! Añadir anexo 
+        .tarjeta.color-secundario.p-3.mb-4
+          .row.justify-content-around.align-items-center
+            .col-auto
+              img(src="@/assets/curso/temas/tema3/31.svg").img65
+            .col
+              .row.justify-content-between.align-items-center
+                .col.mb-3.mb-sm-0
+                  h4.mb-1 Modelos de comunicación y liderazgo
+                  p.mb-0.text-small Le invitamos a consultar el siguiente recurso descargable que le permitirá profundizar en la temática de los procesos de personal en las organizaciones.			
+                .col-sm-auto
+                  a.boton.color-acento-botones(href="https://docs.google.com/document/d/1wD3oByzOhsH4c0oWnUb4DWNBc0hmCOhrMDWG0-nCtyo/edit?usp=sharing" target="_blank")
+                    span Descargar
+                    i.fas.fa-file-download
+      .col-lg-4: img(src='@/assets/curso/temas/tema8/3.svg', alt='')
 
 </template>
 
